fc9219400c2ee2aa05061e34143e83ebcd3874c8
Gavin Hell <contact@gavinhell.com>
2024-03-04 16:25:17 -0800

    lsf feature

--- main.go
+++ main.go
@@ -10,8 +10,28 @@ import (
 func main() {
 	width, _, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting terminal size: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error getting terminal size: %v\n\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("Terminal width: %d columns\n", width)
+	listFilesAndFolders(".")
+}
+
+func listFilesAndFolders(dirPath string) {
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading directory: %v\n", err)
+		return
+	}
+
+	for _, entry := range entries {
+		name := entry.Name()
+		if entry.IsDir() {
+			// Attempt to print directory names in blue
+			fmt.Printf("\033[34m%s\033[0m\n", name)
+		} else {
+			// Print file names in default color
+			fmt.Println(name)
+		}
+	}
 }
