59b0462db692764c979997e561d4e5d43d27c4b5
Gavin Hell <contact@gavinhell.com>
2024-03-11 16:14:53 -0700

    Length flag added -l

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,16 +9,24 @@ import (
 )
 
 func main() {
+	lFlag := flag.Bool("l", false, "List files and folders with their lengths")
+	flag.Parse() // Parse the flags
+
 	width, _, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error getting terminal size: %v\n\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("Terminal width: %d columns\n\n", width)
-	listFilesAndFolders(".")
+
+	if *lFlag {
+		listFilesAndFolders(".", true)
+	} else {
+		listFilesAndFolders(".", false)
+	}
 }
 
-func listFilesAndFolders(dirPath string) {
+func listFilesAndFolders(dirPath string, showLength bool) {
 	entries, err := os.ReadDir(dirPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error reading directory: %v\n", err)
@@ -26,11 +35,15 @@ func listFilesAndFolders(dirPath string) {
 
 	for _, entry := range entries {
 		name := entry.Name()
-		nameLength := len(name)
+		nameData := ""
+		if showLength {
+			nameLength := len(name)
+			nameData = fmt.Sprintf(" | %d", nameLength)
+		}
 		if entry.IsDir() {
-			fmt.Printf("\033[34m%s\033[0m | (%d) \n", name, nameLength)
+			fmt.Printf("\033[34m%s\033[0m%s\n", name, nameData)
 		} else {
-			fmt.Printf("%s | (%d)\n", name, nameLength)
+			fmt.Printf("%s%s\n", name, nameData)
 		}
 	}
 }
